1cc01af1b02a5f6d6d8963e79238655fd6e7afae
watchara hongkim <watchara@punpromotion.com>
2020-03-27 15:10:47 +0700

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const HomePage = props => {
+  return (
+    <div>
+      <h1>Hello COVID-19</h1>
+    </div>
+  )
+}
+
+export default HomePage
